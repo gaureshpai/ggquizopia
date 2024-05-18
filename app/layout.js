d73565c2eee78bfaf6358922a0950eb1393b2f05
paigauresh <paigauresh@gmail.com>
2024-05-19 03:06:21 +0530

    Test and deploy vercel build

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
-import './globals.css'
 import { Inter } from 'next/font/google'
 import { Navigation } from '@/components'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
--- utils/endpoint.js
+++ utils/endpoint.js
@@ -1,3 +1,2 @@
- export const endpoint = 'http://localhost:3000/api/'
-
-// export const endpoint = 'http://ggquizopia.vercel.app/api/'
+// export const endpoint = 'http://localhost:3000/api/'
+export const endpoint = 'https://next-fcc-familyguy.vercel.app/api/'
