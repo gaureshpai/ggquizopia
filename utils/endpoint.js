6388354b4bbc8da363938ab22c14f73a26604610
paigauresh <paigauresh@gmail.com>
2024-05-19 12:03:54 +0530

    improved fetch with a few new profiles

--- utils/endpoint.js
+++ utils/endpoint.js
@@ -1,3 +1,3 @@
-const endpoint = process.env.NEXT_PUBLIC_API_URL || 'https://ggquizopia.vercel.app/api';
+const API_ENDPOINT = process.env.NEXT_PUBLIC_API_URL || 'https://ggquizopia.vercel.app/api';
 
-export { endpoint };
+export { API_ENDPOINT };
